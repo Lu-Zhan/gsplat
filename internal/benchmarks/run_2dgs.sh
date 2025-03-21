657ec74d5af44229883b43a8c0cf784f0499856e
Lu Zhan <zhannixl@gmail.com>
2025-03-21 18:43:07 +0800

    add tonemapping

---
+++ internal/benchmarks/run_2dgs.sh
@@ -0,0 +1,26 @@
+SCENE_DIR="/home/luzhan/Projects/scene_gen/consistent_3dscene/stable-virtual-camera/work_dirs/demo/img2trajvid_s-prob"
+RESULT_DIR="results_meta/consistent_3dscene_2dgs"
+SCENE_LIST="vasedeck/112_views/dense"
+
+for SCENE in $SCENE_LIST;
+do
+    DATA_FACTOR=1
+    echo "Running $SCENE"
+
+    # train without eval
+    # CUDA_VISIBLE_DEVICES=0 python train_3dscene_2dgs.py --eval_steps -1 --disable_viewer --data_factor $DATA_FACTOR \
+    #     --model_type 2dgs \
+    #     --data_dir $SCENE_DIR/$SCENE/ \
+    #     --result_dir $RESULT_DIR/$SCENE/ \
+    #     --max_steps 2000
+
+    # run eval and render
+    for CKPT in $RESULT_DIR/$SCENE/ckpts/*;
+    do
+        CUDA_VISIBLE_DEVICES=0 python train_3dscene_2dgs.py --disable_viewer --data_factor $DATA_FACTOR \
+            --model_type 2dgs \
+            --data_dir $SCENE_DIR/$SCENE/ \
+            --result_dir $RESULT_DIR/$SCENE/ \
+            --ckpt $CKPT
+    done
+done
